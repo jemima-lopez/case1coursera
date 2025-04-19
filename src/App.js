066f90e2c4cc83c4ad8fd4d7c390bf2b53d26b5c
Jemima <jlopezj11@miumg.edu.gt>
2025-04-18 18:39:57 -0600

    commit 7

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import List1 from "./components/List1";
 import List2 from "./components/List2";
 import List3 from "./components/List3";
 import Footer from "./components/Footer";
+import Logo from "./components/Logo";
 import reactImg from "./reactImg.png";
-
-
-function Logo(props) {
-  const logos = <img src={reactImg} height={300}/>;
-  return logos;
-}
+import "./App.css";
 
 function App() {
   const tittle = "Hello World";
@@ -20,11 +16,11 @@ function App() {
       <Nav />
       <Promo />
       <h1 className="tittle">{tittle}</h1>
-      <List1 color="I'm a props"/>{/* how to work one prop */}
-      <List1 color="I'm a props2"/>{/* how to pass props to a component */}
-      <List2 />
-      <List3 />
-      <Logo/>
+      <List1 color="I'm a props" />{/* Demonstrates passing a single prop */}
+      <List1 color="I'm a props2" />{/* Demonstrates passing another prop */}
+      <List2 tittle="My app.com" />{/* Example of passing a prop to List2 */}
+      /* Passing multiple props */
+        <Logo img={reactImg} height={300} description="REACT IMAGEN" />{/* Corrected prop usage for Logo */}
       <Footer />
     </div>
   );
